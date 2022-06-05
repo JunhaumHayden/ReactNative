0a1ad61dc75ff1aea874621b5b36ee941d3ea5c0
Junhaum <junhaumhayden@hotmail.com>
2022-06-05 20:41:17 -0300

    Added in PHP Webpage CSS effects and alert window with JS

--- homepage.php
+++ homepage.php
@@ -1,14 +1,31 @@
 <html>
     <head>
-       <title>Meu primeiro site em PHP!</title>
+       <title>Wooooohooo!!!</title>
+
+       <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+       <style type="text/css">
+           .linha {
+               font-weight: bold;
+               color: green;
+               padding-left: 30px;
+               line-height: 50px;
+           }
+       </style>
     </head>
     
     <body>
        <?php
           for ($i=0; $i < 10 ; $i++) { 
-              print ("Linha Número " .$i . "<br />");
+              print ( "<span class=\"linha\">Linha Número " .$i . "</span><br />");
             } 
         ?>
     </body>
+    <script type="text/javascript"> 
+            $(document).ready(function () {
+               alert("Woooohoooo!!");
+        
+            });
+            </script>
 
 </html>
