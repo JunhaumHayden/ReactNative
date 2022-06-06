4c5ace2ce5591da0470ddd7c9269ccb580c65c0b
Junhaum <junhaumhayden@hotmail.com>
2022-06-05 21:32:27 -0300

    Added in PHP Webpage more CSS effects and accordion widgets using JQuery

--- homepage.php
+++ homepage.php
@@ -1,31 +1,95 @@
-<html>
-    <head>
-       <title>Wooooohooo!!!</title>
-
-       <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-       <style type="text/css">
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Customize icons</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  
+    <style type="text/css">
            .linha {
                font-weight: bold;
                color: green;
-               padding-left: 30px;
-               line-height: 50px;
+               padding-left: 10px;
+               line-height: 30px;
            }
-       </style>
-    </head>
-    
-    <body>
-       <?php
-          for ($i=0; $i < 10 ; $i++) { 
+    </style> 
+    <style type="text/css">
+           .section {
+               font-weight: bold;
+               color: gray;
+               padding-left: 10px;
+               line-height: 30px;
+           }
+    </style>
+
+  <script>
+  $( function() {
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      icons: icons
+    });
+    $( "#toggle" ).button().on( "click", function() {
+      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+        $( "#accordion" ).accordion( "option", "icons", null );
+      } else {
+        $( "#accordion" ).accordion( "option", "icons", icons );
+      }
+    });
+  } );
+  </script>
+</head>
+<body>
+
+        <?php
+          for ($i=0; $i < 5 ; $i++) { 
               print ( "<span class=\"linha\">Linha Número " .$i . "</span><br />");
             } 
         ?>
-    </body>
-    <script type="text/javascript"> 
-            $(document).ready(function () {
-               alert("Woooohoooo!!");
+ 
+<div id="accordion">
+  <h3><span class="section">Section 1</span></h3>
+  <div>
+    <p>Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.</p>
+  </div>
+  <h3><span class="section">Section 2</span></h3>
+  <div>
+    <p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p>
+  </div>
+  <h3><span class="section">Section 3</span></h3>
+  <div>
+    <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+  <h3><span class="section">Section 4</span></h3>
+  <div>
+    <p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+  </div>
+</div>
+ 
+<button id="toggle">Toggle icons</button>
+ 
+ 
+</body>
+
+    <-- <script type="text/javascript"> 
+    <--    $(document).ready(function () {
+    <--        alert("Woooohoooo!!");
         
-            });
-            </script>
+    <--    });
+    <-- </script>
 
 </html>
+
+
+
